8e5054acf42017fc3697524003f88cfaabf15e39
Mancaceresuade <162748921+Mancaceresuade@users.noreply.github.com>
2025-01-27 12:23:15 +0000

    "clase 1 ejercicio 1"

--- clase1/ejemplo1/src/App.java
+++ clase1/ejemplo1/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo!");
+        int numero = 10;
+        System.out.println(numero);
     }
 }
